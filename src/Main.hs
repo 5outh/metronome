8bc3721506129e40168e833c15e004d92cc2b53b
Benjamin Kovach <bkovach5@uga.edu>
2016-05-16 21:00:00 -0400

    smllr

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import System.Directory
 import Control.Exception
 import Control.Concurrent
 import Control.Monad
@@ -22,9 +23,6 @@ data Config = Config
 defaultConfig :: Config
 defaultConfig = Config (4, 4) 120 True
 
-showText :: Show a => a -> Text
-showText = pack . show
-
 delaySeconds :: RealFrac n => n -> IO ()
 delaySeconds n = threadDelay (round $ 1000000 * n)
 
@@ -37,74 +35,40 @@ afplay file = proc "afplay" [file] empty
 toWord :: Int -> Text
 toWord 7 = "sev"
 toWord 11 = "lev"
-toWord 13 = "thirt"
 toWord n = pack (show n)
 
 timeCycle :: (Int, Int) -> Bool -> [Text]
-timeCycle time_@(num, _) ands_ =
-  (if ands_ then (++ ["n"]) . intersperse "n" else id) beats
+timeCycle (num, _) ands_ =
+  if ands_ then intersperse "n" beats ++ ["n"] else beats
   where beats = map toWord [1..num]
-
-cycleDo :: [IO a] -> IO ()
-cycleDo = sequence_ . cycle
 
 -- | Do an action, then (asynchronously) wait the amount of time until the next
 -- beat.
--- I think "say" is taking too long to start or something?
 bpmWait :: Int -> IO a -> IO ()
-bpmWait bpm_ io = do
-  threadId <- forkIO (void io)
-  delaySeconds (60 / fromIntegral bpm_)
-  killThread threadId
+bpmWait bpm_ io =
+  bracket (forkIO (void io)) delay killThread
+  where delay _ = delaySeconds (60 / fromIntegral bpm_)
 
-metronome :: (Text -> IO a) -> Config -> IO ()
-metronome fn Config{..} = do
-  let words_ = timeCycle time ands
-      -- TODO: could obviously be improved
-      bpm_ = if ands then bpm * 2 else bpm
-  mapM_ (bpmWait bpm_ . fn) words_
-
-printMetronome :: Config -> IO ()
-printMetronome = metronome T.putStrLn
-
-sayMetronome :: Config -> IO ()
-sayMetronome config@Config{..} =
-  metronome (say ["--rate=" <> rate]) config
-  where rate = pack (show $ bpm * 3)
-
-  -- /tmp/.sounds/n-120-ands?.aiff
+-- /tmp/.sounds/n-120-ands?.aiff
 fileName :: Config -> Text -> Text
 fileName Config{..} word =
-  mconcat [ "/tmp/.sounds/"
-          , word
-          , "-"
-          , showText bpm
-          , if ands then "-ands" else ""
-          , ".aiff"
-          ]
-
-ignoreFailures :: IO () -> IO ()
-ignoreFailures io = io `catch` handler
-  where
-    handler :: SomeException -> IO ()
-    handler = const $ pure ()
+  "/tmp/.sounds/" <> word <> "-" <> conf <> ".aiff"
+  where conf = pack (show bpm) <> "-" <> if ands then "-a" else ""
 
 genSounds :: Config -> [Text] -> IO SoundMap
 genSounds config@Config{..} words_ = do
-  -- Make the directory for the sounds to go in
-  ignoreFailures $ mkdir "/tmp/.sounds"
+  exists <- doesDirectoryExist "/tmp/.sounds"
+  unless exists $ mkdir "/tmp/.sounds"
 
-  -- Write a .aiff file with the given sounds (permissions problems?)
-  pairs <- forM (nub words_) $ \word -> do
-    let file = fileName config word
-        rate = pack (show $ bpm * 3)
-    code <- say ["--output-file=" <> file, "--rate=" <> rate] word
-    pure (word, file)
-
-  pure $ M.fromList pairs
+  M.fromList <$> mapM sayFile (nub words_)
+  where sayFile word = do
+          let file = fileName config word
+              rate = pack (show $ bpm * 3)
+          void $ say ["--output-file=" <> file, "--rate=" <> rate] word
+          pure (word, file)
 
 cyclePlay :: Config -> [Text] -> SoundMap -> IO ()
-cyclePlay config@Config{..} words_ sounds =
+cyclePlay Config{..} words_ sounds =
   forM_ (cycle words_) $ \word -> do
     let sound = M.lookup word sounds
     case sound of
@@ -116,7 +80,6 @@ cyclePlay config@Config{..} words_ sounds =
 realMetronome :: Config -> IO ()
 realMetronome config@Config{..} = do
   let words_ = timeCycle time ands
-      -- TODO: could obviously be improved
       bpm_ = if ands then bpm * 2 else bpm
   sounds <- genSounds config words_
   cyclePlay config{ bpm = bpm_ } words_ sounds
